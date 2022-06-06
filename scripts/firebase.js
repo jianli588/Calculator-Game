447ee5ec14a6846dfa8eae17a60f5d53ca5d8625
jianli588 <82671025+jianli588@users.noreply.github.com>
2022-06-06 14:24:36 -0400

    finished the basic game, more feature can be added

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-analytics.js";
+import { getDatabase, ref, child, push, update, get, set, query, orderByChild, startAt, endAt, onValue} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,23 +21,117 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+var numberOfValues = 0;
+var levelData;
 
 
 function createAndSave(){
-  console.log("123");
+
+  get(child(ref(database), "/count/")).then((snapshot) => {
+    if (snapshot.exists()) {
+      numberOfValues = snapshot.val().numberOfValue + 1;
+  } else {
+    console.log("No data available");
+    return;
+  }
+  }).catch((error) => {
+  console.error(error);
+  return;
+}).then(() => {
+  set(ref(database, '/count/'), {
+  numberOfValue: numberOfValues
+  })
+  .then(() => {
+  console.log(numberOfValues);
+  var button_text_one = num_pad_button[0].textContent;
+  var button_text_two = num_pad_button[1].textContent;
+  var button_text_three = num_pad_button[2].textContent;
+  var button_text_four = num_pad_button[3].textContent;
+  var button_text_five = num_pad_button[4].textContent;
+  var button_text_six = num_pad_button[5].textContent;
+  var button_text_seven = num_pad_button[6].textContent;
+  var number_of_move = move.length;
+  var moves = move.join("XXXX");
+  var target_number = document.getElementById("calculator_number").value;
+  var starting_number = document.getElementById("starting_number").value;
+
+  const postData = {
+    buttonOne: button_text_one,
+    buttonTwo: button_text_two,
+    buttonThree: button_text_three,
+    buttonFour: button_text_four,
+    buttonFive: button_text_five,
+    buttonSix: button_text_six,
+    buttonSeven: button_text_seven,
+    numberMove: number_of_move,
+    moves: moves,
+    targetNum: target_number,
+    startingNum: starting_number,
+    number: numberOfValues
+  };
+
+  const newPostKey = push(child(ref(database), 'posts')).key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  const updates = {};
+  updates['/levels/' + newPostKey] = postData;
+
+  document.getElementById("created_modal_page_level_seed").value = newPostKey;
+
+  return update(ref(database), updates);
+  })
+  .catch((error) => {
+  console.log(error);
+  })
+});
 }
 
-function copyLevelSeed() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
+function loadRandom(){
+  get(child(ref(database), "/levels/")).then((snapshot) => {
+    if (snapshot.exists()) {
+      levelData = snapshot.val();
+      
+  } else {
 
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    return;
+  }
+  }).catch((error) => {
+  console.error(error);
+  return;
 
-   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+}).then(() =>{
+  console.log("this is stupid");
+  var levelKeys = Object.keys(levelData);
 
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
+  var levelNum = Math.floor(Math.random()*levelKeys.length);
+  loadLevel(levelKeys[levelNum])
+
+});
 }
+
+
+function loadLevel(key){
+  get(child(ref(database), "/levels/" + key +"/")).then((snapshot) => {
+    if (snapshot.exists()) {
+      levelData = snapshot.val();
+      console.log("data");
+  } else {
+    alert("the level seed: " + key + " is incorrect!!:");
+    return;
+  }
+  }).catch((error) => {
+  console.error(error);
+  return;
+}).then(() =>{
+  setLevelData(levelData, key);
+});
+}
+
+
+
+
+window.createAndSave = createAndSave;
+window.loadRandom = loadRandom;
+window.loadLevel = loadLevel;
---
+++ scripts/index_specific.js
@@ -0,0 +1,137 @@
+var move = [];
+var startingNum;
+var targetNum;
+var totalMove;
+var currentMove = 0;
+var computerMove = false;
+
+const topLeftButton = document.getElementById("top_left_button");
+const topButton = document.getElementById("top_button");
+const topRightButton = document.getElementById("top_right_button");
+const leftButton = document.getElementById("left_button");
+const centerButton = document.getElementById("center_button");
+const rightButton = document.getElementById("right_button");
+const bottomButton = document.getElementById("bottom_button");
+const levelKeyScreen = document.getElementById("level_seed_number");
+
+const calculatorScreen = document.getElementById("calculator_screen");
+const targetNumberScreen = document.getElementById("target_number");
+const StepLeftCounter = document.getElementById("steps_left_counter");
+const hintButton = document.getElementById("hint_button");
+
+const messageModal = document.getElementById("message_modal");
+const indexModalText = document.getElementById("index_modal_text");
+const indexModalTitle = document.getElementById("index_modal_title");
+const indexModalButton = document.getElementById("index_modal_button");
+
+function setLevelData(levelData, key){
+    topLeftButton.textContent = levelData.buttonOne;
+    topButton.textContent = levelData.buttonTwo;
+    topRightButton.textContent = levelData.buttonThree;
+    leftButton.textContent = levelData.buttonFour;
+    centerButton.textContent = levelData.buttonfive;
+    rightButton.textContent = levelData.buttonSix;
+    bottomButton.textContent = levelData.buttonSeven;
+    levelKeyScreen.textContent = key.trim();
+
+    startingNum = levelData.startingNum;
+    calculatorScreen.textContent = startingNum;
+    
+    targetNum = levelData.targetNum;
+    targetNumberScreen.textContent = targetNum;
+
+    totalMove = levelData.numberMove;
+    StepLeftCounter.textContent = totalMove;
+    
+
+    move = levelData.moves.split("XXXX");
+}
+
+function after_calculation(command){
+
+    StepLeftCounter.textContent = parseInt(StepLeftCounter.textContent) - 1;
+
+    if (calculatorScreen.textContent === targetNum){
+        //You won Modal pop up
+        indexModalTitle.textContent = "Congratulations";
+        indexModalText.textContent = "You Won!!! You will load a new level by clicking the close button"
+        messageModal.setAttribute("style", "display: block");
+
+
+    }
+
+    else if(StepLeftCounter.textContent === "0"){
+        indexModalTitle.textContent = "you lose!";
+        indexModalText.textContent = "You can retry the level by clicking the close button"
+        messageModal.setAttribute("style", "display: block");
+    }
+
+    else if(!computerMove){
+        if(!hintButton.classList.contains("disabled")){
+            hintButton.classList.add("disabled");
+        }
+    }
+
+
+}
+
+function loadNextLevel(){
+    messageModal.setAttribute("style", "display: none");
+    loadRandom();
+}
+
+function retryLevel(){
+    messageModal.setAttribute("style", "display: none");
+    reset();
+}
+
+function hint(){
+    computerMove = true;
+    calculate(move[currentMove]);
+    currentMove += 1;
+}
+
+function loadNewLevel(){
+    loadLevel(document.getElementById("level_key_input").value);
+}
+
+function reset(){
+    StepLeftCounter.textContent = totalMove;
+
+    if(hintButton.classList.contains("disabled")){
+        hintButton.classList.remove("disabled");
+    }
+    calculatorScreen.textContent = startingNum;
+    currentMove = 0
+
+}
+
+
+function add(num){
+
+    const starting_number = calculatorScreen.textContent;
+    calculatorScreen.textContent = parseInt(starting_number) + parseInt(num);
+    console.log(calculatorScreen.value);
+}
+
+function subtract(num){
+    const starting_number = calculatorScreen.textContent;
+    calculatorScreen.textContent = parseInt(starting_number) - parseInt(num);
+
+}
+
+function multiply(num){
+    const starting_number = calculatorScreen.textContent;
+    calculatorScreen.textContent = parseInt(starting_number) * parseInt(num);
+
+}
+
+function divide(num){
+    const starting_number = calculatorScreen.textContent;
+    calculatorScreen.textContent = (parseInt(starting_number) - (parseInt(starting_number) % parseInt(num))) / parseInt(num);
+}
+
+function reverse(){
+    const starting_number = calculatorScreen.textContent;
+    calculatorScreen.textContent = starting_number.split("").reverse().join("");;
+}
